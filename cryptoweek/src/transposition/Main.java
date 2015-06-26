3c92956edc48a0359cfab57b4d5491fe3821ab08
aminecmi <aminecmi@gmail.com>
2015-06-26 09:02:28 +0200

    encode

--- cryptoweek/src/transposition/Main.java
+++ cryptoweek/src/transposition/Main.java
@@ -1,4 +1,8 @@
 package transposition;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("VOICI UN MESSAGE CLAIR");
+        System.out.println(TranspoCypher.encode("VOICI UN MESSAGE CLAIR"));
+    }
 }
--- cryptoweek/src/transposition/TranspoCypher.java
+++ cryptoweek/src/transposition/TranspoCypher.java
@@ -1,4 +1,22 @@
 package transposition;
 
 public class TranspoCypher {
+    static int[] key = {2, 4, 0, 1, 3};
+
+    public static String encode(String message) {
+        String encoded = "";
+        int i=0;
+        int limit = key.length - ((int) message.length() % key.length);
+        for (int k = 0; k < limit; k++)
+            message += " ";
+        int length = message.length();
+        while (i < length) {
+            for(int j= 0; j< key.length; j++) {
+                encoded += message.charAt(key[j]);
+            }
+            message = message.substring(key.length);
+            i += key.length;
+        }
+        return encoded;
+    }
 }
